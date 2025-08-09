158851b778fcb4da4c6dfe426b2ebed57c5269b8
AhmedMosalam1 <ahmed.mosalam161@gmail.com>
2025-08-09 20:11:22 +0300

    add tools & description

--- models/projectModel.js
+++ models/projectModel.js
@@ -1,51 +1,55 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const slugify = require('slugify');
 
-
-const projectSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:[true,"Project must be required"],
-        unique:[true,"Project must be unique"],
-        minLength:[3,"Too Short Project Name"],
-        maxLength:[32,"Too Long Project Name"]
+const projectSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Project must be required'],
+      unique: [true, 'Project must be unique'],
+      minLength: [3, 'Too Short Project Name'],
+      maxLength: [32, 'Too Long Project Name'],
     },
-    slug:{
-        type:String,
-        unique:true,
-        lowercase:true,
+    slug: {
+      type: String,
+      unique: true,
+      lowercase: true,
     },
-    projectImage:{
-        type:String,
-        //required: [true, "a project must have image"]
+    projectImage: {
+      type: String,
+      //required: [true, "a project must have image"]
     },
-    linkProject:{
-        type:String,
-        required: [true, "a project must have link"]
+    linkProject: {
+      type: String,
+      required: [true, 'a project must have link'],
     },
-    linkProjectGH:{
-        type:String,
-       // required: [true, "a project must have github link"]
+    linkProjectGH: {
+      type: String,
+      // required: [true, "a project must have github link"]
     },
-    status:{
-        type:String
+    status: {
+      type: String,
     },
-    tech:{
-        type:String
+    tech: {
+      type: String,
     },
-    techImage:[String],
-    
-},
-{
-    timestamps:true,
-})
+    techImage: [String],
+    tools: [String],
+    description: {
+      type: String,
+      //required:[true,"Project must be required"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 projectSchema.pre('save', function (next) {
-    this.slug = slugify(this.name, { lower: true })
-    next()
-})
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
 
+const projectModel = mongoose.model('Project', projectSchema);
 
-const projectModel = mongoose.model('Project',projectSchema); 
-
-module.exports = projectModel
+module.exports = projectModel;
